a7a56f1dd03b463a5ae119b3734e523083afaaed
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-05-06 14:57:55 +0700

    Praktikum 3: Membuat Fitur Export PDF

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Barryvdh\DomPDF\Facade\PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -106,5 +107,11 @@ class ArticleController extends Controller
     public function destroy(Article $article)
     {
         //
+    }
+
+    public function cetak_pdf(){
+        $articles = Article::all();
+        $pdf = PDF::loadview('articles.articles_pdf',['articles'=>$articles]);
+        return $pdf->stream();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
 Route::resource('articles',ArticleController::class);
+Route::get('article/cetak_pdf',[ArticleController::class, 'cetak_pdf']);
+
 Route::get('/', function () {
     return view('welcome');
 });
